314bcb31e7bfce8b4f654cc8313e25ba54d425ea
1331-23 <leron2513@gmail.com>
2026-02-07 16:56:49 +0300

    update 0.0.1

--- app/src/main/java/com/example/otkritka_lb2/MainActivity.kt
+++ app/src/main/java/com/example/otkritka_lb2/MainActivity.kt
@@ -1,20 +1,92 @@
 package com.example.otkritka_lb2
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.otkritka_lb2.ui.theme.Otkritka_Lb2Theme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContent {
+            Otkritka_Lb2Theme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GreetingText(
+                        message = "Happy Birthday Alina!",
+                        from = "From Takhir",
+                        modifier = Modifier.padding(8.dp)
+                    )
+                }
+            }
         }
+    }
+}
+
+@Composable
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // Время события
+        Text(
+            text = "12:00",
+            fontSize = 24.sp,
+            color = MaterialTheme.colorScheme.secondary,
+            modifier = Modifier.padding(bottom = 32.dp)
+        )
+
+
+        // Основное поздравление
+        Text(
+            text = message,
+            fontSize = 36.sp,
+            lineHeight = 40.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+
+        // Подпись
+        Text(
+            text = from,
+            fontSize = 24.sp,
+            color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(Alignment.End)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingTextPreview() {
+    Otkritka_Lb2Theme {
+        GreetingText(
+            message = "Happy " +
+                    "Birthday " +
+                    "Alina!",
+            from = "From Takhir"
+        )
     }
 }
